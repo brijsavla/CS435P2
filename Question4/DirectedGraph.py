b222432db8d1ca6e812650dcad89302530daf7ac
brijsavla <bs375@njit.edu>
2020-05-11 16:40:06 -0400

    Create DirectedGraph.py

---
+++ Question4/DirectedGraph.py
@@ -0,0 +1,23 @@
+class GraphNode:
+  def __init__(self, val):
+    self.val = val
+    self.neighbors = list()
+
+class Graph:
+  def __init__(self):
+    self.vertices = list()
+
+  def addNode(self, nodeValue):
+    nodeToAdd = GraphNode(nodeValue)
+    self.vertices.append(nodeToAdd)
+
+  def addDirectedEdge(self, first, second):
+    if first not in second.neighbors:
+      first.neighbors.append(second)
+
+  def removeDirectedEdge(self, first, second):
+    if first in second.neighbors:
+      first.neighbors.remove(second)
+
+  def getAllNodes(self):
+    return self.vertices
